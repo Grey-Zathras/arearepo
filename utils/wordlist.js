45eb5a3c5cbc16ab20d479af8ed5585415924d47
Grey-Zathras <greyzathras@mail.ru>
2024-04-30 19:34:53 +0300

    wordlist path corrected

--- utils/wordlist.js
+++ utils/wordlist.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const fullWordList = fs.readFileSync('wordlist.txt', 'utf-8');
+const fullWordList = fs.readFileSync('..\wordlist.txt', 'utf-8');
 const wordList = fullWordList.split(/\r?\n/);
 
 module.exports = wordList;
