4efc934e046acabe03a62aacd042f7317d28aaa8
Grey-Zathras <greyzathras@mail.ru>
2024-05-18 23:03:21 +0300

    generateSpies: killers may be spies

--- utils/card_generator.js
+++ utils/card_generator.js
@@ -7,12 +7,12 @@ function getRandomInt(max) {
     const maxwords=wordList.length;
     const number_list = [];
     const cards = [25];
-    for (var i=0; i<maxwords;i++){
+    for (let i=0; i<maxwords;i++){
       number_list[i]=i;
     }
-    var limit=maxwords; 
-    for (var i=0; i<25;i++){
-      var n=getRandomInt(limit);
+    let limit=maxwords; 
+    for (let i=0; i<25;i++){
+      let n=getRandomInt(limit);
       limit--;
       cards[i]=wordList[ number_list[n] ];
       number_list[n]=number_list[limit];
@@ -22,66 +22,82 @@ function getRandomInt(max) {
   }
 
   exports.generateSpies = function (){
-    const number_list = [];
+    //const number_list = [];
     const spies = [[],[]];
+    const maybe_killers = [[],[]];
     const cards = [[],[] ];
     // preparations before shuffle
-    for (var j=0; j<2;j++){
-        for (var i=0; i<25;i++){
-        number_list[i]=i;
+    for (let j=0; j<2;j++){
+        for (let i=0; i<25;i++){
+        //number_list[i]=i;
         cards[j][i]=i;
         }
     }
 
-    var limit=25; 
+    let limit=25; 
     //generate(shuffle) the spies shared cards (0-2)
-    for (var i=0; i<3;i++){
-        var n=getRandomInt(limit);
+    for (let i=0; i<3;i++){
+        let n=getRandomInt(limit);
         limit--;
-        for (var j=0; j<2;j++){
+        for (let j=0; j<2;j++){
             spies[j][i]=cards[j][n];
             cards[j][n] = cards[j][limit];
             cards[j][limit]=spies[j][i];
         }
-    }    
+    } 
+    // killers not shared with spies   
+    let mk_limit=limit;// 16; // 25 -9
+    for (let j=0; j<2;j++){
+        for (let i=0; i< mk_limit;i++){
+            maybe_killers[j][i]=cards[j][i];
+        }
+    }
     //generate(shuffle) the spies unique cards (3-8)
-    for (var j=0; j<2;j++){
-        for (var i=3; i<9;i++){
-            var n=getRandomInt(limit);
+    for (let i=3; i<9;i++){
+        mk_limit--;
+        for (let j=0; j<2;j++){
+            let n=getRandomInt(limit);
             limit--;
             spies[j][i]=cards[j][n];
             cards[j][n] = cards[j][limit];
+            maybe_killers[j][n]=cards[j][limit];
             cards[j][limit]=spies[j][i];
             cards[1-j][n] = cards[1-j][limit];
-            cards[1-j][limit]=spies[1-j][i];
+            cards[1-j][limit]=spies[j][i];
         }
     }
     //generate(shuffle) the killers (9-11), randomly shared or not
-    for (var i=9; i<12;i++){
-        limit--;
-        for (var j=0; j<2;j++){
-            var n=getRandomInt(limit+1);
-            spies[j][i]=cards[j][n];
-            cards[j][n] = cards[j][limit];
-            cards[j][limit]=spies[j][i];
+    for (let i=9; i<12;i++){
+        mk_limit--;
+        for (let j=0; j<2;j++){
+            let n=getRandomInt(mk_limit+1);
+            spies[j][i]=maybe_killers[j][n];
+            maybe_killers[j][n] = maybe_killers[j][mk_limit];
+            maybe_killers[j][mk_limit]=spies[j][i];
         }
     }
-
-    for (var j=0; j<2;j++){
-        for (var i=0; i<25;i++){
+    //reuse cards array - nullify
+    for (let j=0; j<2;j++){
+        for (let i=0; i<25;i++){
             cards[j][i]=0;
         }
     }
-    for (var j=0; j<2;j++){
-        for (var i=0; i<9;i++){
+    for (let j=0; j<2;j++){
+        for (let i=0; i<9;i++){
             cards[j][spies[j][i]]=1;
         }
-        for (var i=9; i<12;i++){
+        for (let i=9; i<12;i++){
             cards[j][spies[j][i]]=2;
         }
     }
+    /*
     console.log(spies[0]);
     console.log(spies[1]);
+    const filteredArray = spies[0].filter(value => spies[1].includes(value));
+    console.log(filteredArray);
+    console.log(spies[0].sort(function(a, b){return a - b}));
+    console.log(spies[1].sort(function(a, b){return a - b}));
+    */
     return cards;
   }
 
